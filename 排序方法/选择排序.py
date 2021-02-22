0f6fc6272c470c3d5159a3fa9cffa32113630fa0
zhourx1129 <534418624@qq.com>
2021-02-22 21:03:07 +0800

    python

--- 排序方法/选择排序.py
+++ 排序方法/选择排序.py
@@ -1,9 +1,9 @@
 def SelectSort(li):
     for i in range(len(li)-1):
-        min_num = i
+        # min_num = i
         for j in range(i+1,len(li)):
-            if li[j] < li[min_num]:
-                li[j],li[min_num] = li[min_num],li[j]
+            if li[j] < li[i]:
+                li[j],li[i] = li[i],li[j]
         print(li)
 
 li = [9,5,3,7,6,8,1,2,4]
