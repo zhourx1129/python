f907b676aac1380592da509507c95ae26a8d9a1a
zhourx1129 <534418624@qq.com>
2021-02-22 22:14:30 +0800

    python

--- 排序方法/插入排序.py
+++ 排序方法/插入排序.py
@@ -1,7 +1,7 @@
 def InsertionSort(li):
     n = len(li)
     for i in range(1,n):
-        tem = li[i]
+        # tem = li[i]
         for j in range(i,0,-1):
             if li[j-1] > li[j]:
                 li[j-1],li[j] = li[j],li[j-1]
