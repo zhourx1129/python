464ba784a870f17f7a931362f89dea67b765d0c2
zhourx1129 <534418624@qq.com>
2021-06-06 00:04:46 +0800

    update 前期简易爬虫/douban book.py.

--- 前期简易爬虫/douban book.py
+++ 前期简易爬虫/douban book.py
@@ -14,8 +14,9 @@ headers = {
     'User-Agent':UserAgent().random
 }
 url = 'https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4'
-response = requests.get(url=url,headers=headers).text
-tree = etree.HTML(response)
+response = requests.get(url=url,headers=headers)
+response.encoding = 'utf-8'
+tree = etree.HTML(response.text)
 li_list = tree.xpath('//*[@id="subject_list"]/ul/li')
 for li in li_list:
     detailed_information = li.xpath('./div[2]/div/text()')[0].strip()  #详细信息
@@ -23,5 +24,5 @@ for li in li_list:
     synopsis = li.xpath('./div[2]/p/text()')[0].strip()  #简介
     score = li.xpath('./div[2]/div/span[2]/text()')[0].strip() #评分
     with open('./豆瓣图书.txt','a',encoding='utf-8') as fp:
-        fp.write(book_name+'\t'+score+'\t'+detailed_information+'\n'+'\t'+synopsis)
+        fp.write(book_name+'\t'+score+'\t'+detailed_information+'\n'+'\t'+synopsis+'\n'+'\n')
         print(book_name,'已完成')
